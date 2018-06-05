9efbd4a9c0118549f497ab70c57e74920fcf4e5d
REXOTIUM <rick.pie@hotmail.com>
2018-06-05 12:40:51 +0200

    Update LobbyController.java

--- app/controllers/LobbyController.java
+++ app/controllers/LobbyController.java
@@ -10,13 +10,11 @@ import dal.repositories.SessionRepository;
 import play.db.jpa.Transactional;
 import play.mvc.Controller;
 import play.mvc.Result;
-
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
 @Singleton
 public class LobbyController extends Controller {
-
     private final GameRepository gameRepository;
     private final SessionRepository sessionRepository;
     private ObjectMapper mapper;
@@ -31,24 +29,18 @@ public class LobbyController extends Controller {
     @Transactional
     public Result addToQueue() {
         JsonNode body = request().body().asJson();
-
         String uuid = body.get("sessionId").asText();
-
         ObjectNode json = mapper.createObjectNode();
-
         // Voorkom dubbele sessions
         Session gameSession = sessionRepository.findOne(uuid);
         if (gameSession != null) {
             Game game = gameRepository.getOpenGame();
             gameRepository.saveGame(game);
             game.addSession(gameSession);
-
             json.put("sessionId", gameSession.getId());
             json.put("gameId", game.getId());
             json.put("size", game.getSessions().size());
         }
-
         return ok(json.toString());
-
     }
 }
