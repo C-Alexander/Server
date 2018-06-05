dee319cd713d4775362b856916dc157e6f60e53c
REXOTIUM <rick.pie@hotmail.com>
2018-06-05 12:44:10 +0200

    Update Game.java

--- app/dal/entities/Game.java
+++ app/dal/entities/Game.java
@@ -1,6 +1,6 @@
 package dal.entities;
-import org.hibernate.annotations.GenericGenerator;
 
+import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +12,10 @@ import java.util.List;
         @NamedQuery(name = "Game.findFirstOpen", query = "select g from Game as g where (SELECT COUNT(*) FROM g.sessions as gSession) < 2")
 })
 public class Game {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private String id;
     @OneToMany(cascade=CascadeType.ALL, orphanRemoval = true)
     @JoinTable(
             joinColumns = @JoinColumn(name = "gameId", referencedColumnName = "id"),
@@ -22,11 +26,6 @@ public class Game {
         sessions = new ArrayList<>();
     }
 
-    @Id
-    @GeneratedValue(generator = "system-uuid")
-    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
-    private String id;
-
     public String getId() {
         return id;
     }
@@ -35,10 +34,15 @@ public class Game {
         this.id = id;
     }
 
-    public List<Session> getSessions() { return this.sessions; }
+    public List<Session> getSessions() {
+            return this.sessions;
+    }
 
-    public void addSession(Session session) { this.sessions.add(session); }
+    public void addSession(Session session) {
+            this.sessions.add(session);
+    }
 
-    public void removeSession(Session session) { this.sessions.remove(session); }
+    public void removeSession(Session session) {
+            this.sessions.remove(session);
+    }
 }
-
